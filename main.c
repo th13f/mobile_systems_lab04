88bf5739ae9234a394985d145613d5b06e3cccb6
Roman <fpm.th13f@gmail.com>
2013-04-11 20:53:06 +0300

    written funcs for open, release, write

--- main.c
+++ main.c
@@ -37,10 +37,22 @@ static struct class** classes;
 
 static int device_open(struct inode *inode, struct file *file)
 {
+        if (device_opened)
+                return -EBUSY;
+
+        device_opened++;
+        try_module_get(THIS_MODULE);
+
+        return 0;
 }
 
 static int device_release(struct inode *inode, struct file *file)
 {
+        device_opened--;
+
+        module_put(THIS_MODULE);
+
+        return 0;
 }
 
 static ssize_t device_read( struct file *filp, char *buffer, size_t length, loff_t * offset)
@@ -49,6 +61,45 @@ static ssize_t device_read( struct file *filp, char *buffer, size_t length, loff
 
 static ssize_t device_write(struct file *filp, const char *buff, size_t len, loff_t * off)
 {
+        static int fin = 0;
+        char name[32];
+        int buf_size = 0;
+        int index = 0;
+
+        if (fin) {
+                fin = 0;
+                return 0;
+        }
+
+        strcpy(name, filp->f_dentry->d_name.name);
+
+        if (strcmp(name, CALC_FIRST) == 0) {
+                index = 0;
+        } else if (strcmp(name, CALC_SECOND) == 0) {
+                index = 1;
+        } else if (strcmp(name, CALC_OPERATOR) == 0) {
+                index = 2;
+        } else {
+                index = 3;
+        }
+
+        if (len >= FILE_MAX_SIZE) {
+                buf_size = FILE_MAX_SIZE - 1;
+        } else {
+                buf_size = len;
+        }
+
+        if (index != 3) {
+                if (copy_from_user(devices_buffer[index], buff, buf_size)) {
+                                printk(KERN_ERR "Failed to write to /dev/%s\n", names[index]);
+                                return -EFAULT;
+                }
+                devices_buffer[index][buf_size + 1] = '\0';
+        }
+
+        fin = 1;
+
+        return buf_size;
 }
 
 /* Module init function */
