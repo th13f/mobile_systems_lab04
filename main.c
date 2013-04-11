606ec2311e6690bfda65fafe77d0ca70a7f4256a
Roman <fpm.th13f@gmail.com>
2013-04-11 21:49:44 +0300

    added some bodies of funcs

--- main.c
+++ main.c
@@ -1,3 +1,13 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/version.h>
+#include <linux/types.h>
+#include <linux/kdev_t.h>
+#include <linux/device.h>
+#include <linux/cdev.h>
+#include <linux/init.h>
+#include <linux/slab.h>
+#include <asm/uaccess.h>
 #include "main.h"
 
 /* Char devices files names. */
@@ -57,6 +67,67 @@ static int device_release(struct inode *inode, struct file *file)
 
 static ssize_t device_read( struct file *filp, char *buffer, size_t length, loff_t * offset)
 {
+        static int fin = 0;
+        long a, b, result = 0;
+        char name[32], op = 0, *end;
+        int written = 0;
+        int i = 0;
+
+        if (fin) {
+                fin = 0;
+                return 0;
+        }
+
+        strcpy(name, filp->f_dentry->d_name.name);
+
+        if (strcmp(name, CALC_FIRST) == 0) {
+                sprintf(message, "\n%s\n", devices_buffer[0]);
+        } else if (strcmp(name, CALC_SECOND) == 0) {
+                sprintf(message, "\n%s\n", devices_buffer[1]);
+        } else if (strcmp(name, CALC_OPERATOR) == 0) {
+                sprintf(message, "\n%s\n", devices_buffer[2]);
+        } else {
+                a = simple_strtol(devices_buffer[0], &end, 10);
+                if (a == 0 && (*end == devices_buffer[0][0])) {
+                        sprintf(message, "\n%s\n", "First operand is not integer.");
+                } else {
+                        b = simple_strtol(devices_buffer[1], &end, 10);
+                        if (b == 0 && (*end == devices_buffer[1][0])) {
+                                sprintf(message, "\n%s\n", "Second operand is not integer.");
+                        } else {
+                                op = devices_buffer[2][0];
+                                switch (op) {
+                                        case '+': result = a + b; break;
+                                        case '-': result = a - b; break;
+                                        case '*': result = a * b; break;
+                                        case '/':
+                                                if (b == 0) {
+                                                        sprintf(message, "\n%s\n",
+                                                                "Division by zero!");
+                                                        written = 1;
+                                                        break;
+                                                }
+                                                result = a / b;
+                                                break;
+                                        default:
+                                                sprintf(message, "\nUnknown operand: %c\n", op);
+                                                written = 1;
+                                                break;
+                                }
+                        }
+                        if (!written) {
+                                sprintf(message, "\n%ld %c %ld = %ld\n", a, op, b, result);
+                        }
+                }
+        }
+
+        for(i = 0; i < length && message[i]; i++) {
+                put_user(message[i], buffer + i);
+        }
+
+        fin = 1;
+
+        return i;
 }
 
 static ssize_t device_write(struct file *filp, const char *buff, size_t len, loff_t * off)
@@ -105,6 +176,47 @@ static ssize_t device_write(struct file *filp, const char *buff, size_t len, lof
 /* Module init function */
 static int __init calc_init(void)
 {
+        int i = 0;
+
+        printk(KERN_INFO "Calc driver was loaded.\n");
+
+        classes = (struct class**) kmalloc(sizeof(struct class*) * 4, GFP_KERNEL);
+        c_dev = (struct cdev*) kmalloc(sizeof(struct cdev) * 4, GFP_KERNEL);
+        devices_buffer = (char**) kmalloc(sizeof(char*) * 4, GFP_KERNEL);
+
+        for (i = 0; i < 4; i++) {
+                devices_buffer[i] = (char*) kmalloc(sizeof(char) * FILE_MAX_SIZE, GFP_KERNEL);
+                devices_buffer[i][0] = '\0';
+        }
+
+        for (i = 0; i < 4; i++) {
+                if (alloc_chrdev_region(&numbers[i], 0, 1, names[i]) < 0) {
+                        return -1;
+                }
+
+                if ((classes[i] = class_create(THIS_MODULE, names[i])) == NULL) {
+                        unregister_chrdev_region(numbers[i], 1);
+                        return -1;
+                }
+
+                if (device_create(classes[i], NULL, numbers[i], NULL, names[i]) == NULL) {
+                        class_destroy(classes[i]);
+                        unregister_chrdev_region(numbers[i], 1);
+                        return -1;
+                }
+
+                cdev_init(&c_dev[i], &fops);
+
+                if (cdev_add(&c_dev[i], numbers[i], 1) == -1) {
+                        device_destroy(classes[i], numbers[i]);
+                        class_destroy(classes[i]);
+                        unregister_chrdev_region(numbers[i], 1);
+                        return -1;
+                }
+        }
+
+        printk(KERN_INFO "Calc driver devices were created.\n");
+        return 0;
 }
 
 /* Module exit function */
